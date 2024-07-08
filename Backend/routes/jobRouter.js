3265047addba65d1c8014d196f20b0c5b5938a0a
Samar Yadav <samarpratapjnv046@gmail.com>
2024-07-08 10:10:16 +0530

    fift commit

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteJob, getAllJobs, getMyJobs, postJob, updateJob} from '../controllers/jobController.js'
+import {deleteJob, getAllJobs, getMyJobs, postJob, updateJob,getSingleJob} from '../controllers/jobController.js'
 import {isAuthorized} from '../middlewares/auth.js'
 
 const router =express.Router();
@@ -9,5 +9,6 @@ router.post("/post",isAuthorized,postJob);
 router.get("/getmyjobs",isAuthorized,getMyJobs);
 router.put("/update/:id",isAuthorized,updateJob);
 router.put("/delete/:id",isAuthorized,deleteJob);
+router.get("/:id", isAuthorized, getSingleJob);
 
 export default router;
